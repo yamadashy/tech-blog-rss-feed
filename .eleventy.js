f1e2dde2b955e32f224736721f7c7e9fc723bb08
Kazuki Yamada <koukun0120@gmail.com>
2022-02-13 20:28:37 +0900

    update: OGP画像をダウンロードして使う

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,63 @@
-const htmlmin = require("html-minifier-terser");
+const htmlmin = require('html-minifier-terser');
+const Image = require('@11ty/eleventy-img');
+
+Image.concurrency = 20;
+
+const minifyHtmlTransform = function (content, outputPath) {
+  if(outputPath && outputPath.endsWith('.html')) {
+    return htmlmin.minify(content,  {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      maxLineLength: 200
+    });
+  }
+
+  return content;
+}
+
+const imageShortcode = async function (src, alt, sizes) {
+  let metadata = null;
+
+  try {
+    metadata = await Image(src, {
+      widths: [300, 600],
+      formats: ['jpeg'],
+      outputDir: 'src/site/images/feed-thumbnails',
+      urlPath: 'images/feed-thumbnails/'
+    });
+  } catch (e) {
+    // エラーが起きたらそのまま使う
+    return `<img src='${src}' alt='${alt}' loading='lazy' decoding='async'>`
+  }
+
+  return Image.generateHTML(metadata, {
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async',
+  });
+}
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/site/images");
-  eleventyConfig.addPassthroughCopy("src/site/feeds");
-  eleventyConfig.addTransform("minify html", function(content, outputPath) {
-    if(outputPath && outputPath.endsWith(".html")) {
-      return htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-        maxLineLength: 200
-      });
-    }
+  // static assets
+  eleventyConfig.addPassthroughCopy('src/site/images');
+  eleventyConfig.addPassthroughCopy('src/site/feeds');
 
-    return content;
-  });
+  // images
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+
+  // minify
+  eleventyConfig.addTransform('minify html', minifyHtmlTransform);
 
   return {
     htmlTemplateEngine: 'njk',
 
     dir: {
-      input: "src/site",
-      output: "public"
+      input: 'src/site',
+      output: 'public'
     }
   }
 }
