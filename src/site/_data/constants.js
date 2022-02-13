434d32bf9a1d4fc81b7396aa34d39bf696d0d6bf
Kazuki Yamada <koukun0120@gmail.com>
2022-02-13 11:36:33 +0900

    update: GA系の値がなければタグを入れないように

--- src/site/_data/constants.js
+++ src/site/_data/constants.js
@@ -1,20 +1,24 @@
 const siteUrl = 'https://yamadashy.github.io/tech-blog-rss-feed/';
 
 module.exports = {
-  author: 'yamadashy',
+  // サイト設定
   siteUrl: `${siteUrl}`,
   siteTitle: '企業テックブログRSS',
   siteDescription:
     '企業のテックブログの更新をまとめたRSSフィードを配信しています。記事を読んでその企業の技術・カルチャーを知れることや、質の高い技術情報を得られることを目的としています。',
-  gitHubUserUrl: 'https://github.com/yamadashy/',
-  gitHubRepositoryUrl: 'https://github.com/yamadashy/tech-blog-rss-feed/',
   feedUrls: {
     atom: `${siteUrl}feeds/atom.xml`,
     rss: `${siteUrl}feeds/rss.xml`,
     json: `${siteUrl}feeds/feed.json`,
   },
-  // Google Search Console
+
+  // GitHub
+  author: 'yamadashy',
+  gitHubUserUrl: 'https://github.com/yamadashy/',
+  gitHubRepositoryUrl: 'https://github.com/yamadashy/tech-blog-rss-feed/',
+
+  // Google Analytics系。フォークして使う際は値を空にするか書き換えてください
   googleSiteVerification: 'GPLvXv8kYtLMW912ZS54DKFEZL6ruOrjOFLdHVTo37o',
-  // Google Analytics
   globalSiteTagKey: 'G-CNNNTL0NB3',
+
 };
