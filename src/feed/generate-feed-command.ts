3a05254234ec65a5d78249c450510a14734a10cd
Kazuki Yamada <koukun0120@gmail.com>
2022-02-18 00:01:37 +0900

    update: フィード取得の並列数を上げる

--- src/feed/generate-feed-command.ts
+++ src/feed/generate-feed-command.ts
@@ -4,8 +4,8 @@ import { FeedGenerator } from './utils/feed-generator';
 import * as fs from 'fs/promises';
 import * as path from 'path';
 
-const FEED_FETCH_CONCURRENCY = 20;
-const FEED_OGP_FETCH_CONCURRENCY = 20;
+const FEED_FETCH_CONCURRENCY = 50;
+const FEED_OGP_FETCH_CONCURRENCY = 50;
 const FILTER_ARTICLE_DATE = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
 const OUTPUT_FEED_DIR_PATH = path.join(__dirname, '../site/feeds');
 const MAX_FEED_DESCRIPTION_LENGTH = 200;
