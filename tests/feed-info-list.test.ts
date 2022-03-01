3a2bdada6a1c15622ba2801e84f646eb55eb5318
Kazuki Yamada <koukun0120@gmail.com>
2022-03-01 13:37:09 +0900

    update: テストでのフィードの取得はjestのconcurrentで並列実行するように

--- tests/feed-info-list.test.ts
+++ tests/feed-info-list.test.ts
@@ -1,41 +1,34 @@
-import { FEED_INFO_LIST } from '../src/resources/feed-info-list';
+import { FEED_INFO_LIST, FeedInfo } from '../src/resources/feed-info-list';
 import * as RssParser from 'rss-parser';
 import { FeedCrawler } from '../src/feed/utils/feed-crawler';
-import { PromisePool } from '@supercharge/promise-pool';
 import * as retry from 'async-retry';
 
+const rssParser = new RssParser();
+
+// 設定のテスト
 test('FEED_INFO_LIST の設定が正しい', () => {
   expect(() => {
     FeedCrawler.validateFeedInfoList(FEED_INFO_LIST);
   }).not.toThrow();
 });
 
-test(
-  'フィードがすべて取得可能',
-  () => {
-    const rssParser = new RssParser();
-    const fetchErrors: Error[] = [];
+// フィード取得テスト
+// TODO: test.concurrent.each が使えたらそれを使いたい
+FEED_INFO_LIST.map((feedInfo: FeedInfo) => {
+  test.concurrent(
+    `フィードが取得可能 - ${feedInfo.label} / ${feedInfo.url}`,
+    async () => {
+      const feed = await retry(
+        async () => {
+          return rssParser.parseURL(feedInfo.url);
+        },
+        {
+          retries: 3,
+        },
+      );
 
-    return PromisePool.for(FEED_INFO_LIST)
-      .withConcurrency(20)
-      .handleError(async (error, feedInfo, pool) => {
-        fetchErrors.push(error);
-        pool.stop();
-      })
-      .process(async (feedInfo) => {
-        const feed = await retry(
-          async () => {
-            return rssParser.parseURL(feedInfo.url);
-          },
-          {
-            retries: 3,
-          },
-        );
-        expect(feed.items.length).toBeGreaterThanOrEqual(0);
-      })
-      .then(() => {
-        expect(fetchErrors).toHaveLength(0);
-      });
-  },
-  60 * 1000,
-);
+      expect(feed.items.length).toBeGreaterThanOrEqual(0);
+    },
+    15 * 1000,
+  );
+});
