56291c48e80c34ac3c61d56db68e59a9d40a26f2
Kazuki Yamada <koukun0120@gmail.com>
2022-03-18 00:19:44 +0900

    update: 人気ページのソート順調整

--- src/site/_data/feedItemsHot.js
+++ src/site/_data/feedItemsHot.js
@@ -9,6 +9,8 @@ dayjs.extend(require('dayjs/plugin/utc'));
 dayjs.locale('ja');
 dayjs.tz.setDefault('Asia/Tokyo');
 
+const FEED_ITEM_FILTER_DAY = 7;
+
 module.exports = async () => {
   const feedData = JSON.parse(await fs.readFile(path.join(__dirname, '../feeds/feed.json')));
 
@@ -17,7 +19,7 @@ module.exports = async () => {
   // 「直近1週間分」かつ「はてなブックマークされている」
   feedItems = feedItems
     .filter((feedItem) => {
-      return dayjs(feedItem.date_published) > dayjs().subtract(7, 'd');
+      return dayjs(feedItem.date_published) > dayjs().subtract(FEED_ITEM_FILTER_DAY, 'd');
     })
     .filter((feedItem) => {
       return feedItem._custom.hatenaCount > 0;
@@ -27,12 +29,16 @@ module.exports = async () => {
   for (const feedItem of feedItems) {
     feedItem.diffDateForHuman = dayjs().to(feedItem.date_published);
     feedItem.pubDateForHuman = dayjs(feedItem.date_published).tz().format('YYYY-MM-DD HH:mm:ss');
+
+    // ソート用の数値。日が建つほど優先度が低くなる
+    const feedItemDiffDays = dayjs().tz().diff(feedItem.date_published, 'day');
+    const feedItemPriorityFactor = Math.max(0.1, (FEED_ITEM_FILTER_DAY - feedItemDiffDays) / FEED_ITEM_FILTER_DAY);
+    feedItem.priorityForSort = feedItem._custom.hatenaCount * feedItemPriorityFactor;
   }
 
-  // はてなブックマース数順でソート
-  // TODO: 単純なソートではなく新着優先しつつの人気順にしたい
+  // ソート
   feedItems = feedItems.sort((a, b) => {
-    return b._custom.hatenaCount - a._custom.hatenaCount;
+    return b.priorityForSort - a.priorityForSort;
   });
 
   return feedItems;
