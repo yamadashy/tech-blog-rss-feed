50e4d7d2697c86cfde6f5d1b7c13e094250df1ca
Y's_上岡 <ys_kamioka@Ys-ueokanoMacBook-Air.local>
2024-01-03 22:25:27 +0900

    change: 設定ファイルをcami-craneのGitHubに変更

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-const siteUrlStem = 'https://yamadashy.github.io/tech-blog-rss-feed';
+const siteUrlStem = 'https://cami-crane.github.io/tech-blog-rss-feed';
 const siteUrl = `${siteUrlStem}/`;
 
 module.exports = {
@@ -13,8 +13,8 @@ module.exports = {
   feedTitle: '企業テックブログRSS',
   feedDescription: '企業のテックブログの更新をまとめたRSSフィード',
   feedLanguage: 'ja',
-  feedCopyright: 'yamadashy/tech-blog-rss-feed',
-  feedGenerator: 'yamadashy/tech-blog-rss-feed',
+  feedCopyright: 'cami-crane/tech-blog-rss-feed',
+  feedGenerator: 'cami-crane/tech-blog-rss-feed',
   feedUrls: {
     atom: `${siteUrl}feeds/atom.xml`,
     rss: `${siteUrl}feeds/rss.xml`,
@@ -22,18 +22,18 @@ module.exports = {
   },
 
   // GitHub
-  author: 'yamadashy',
-  gitHubUserUrl: 'https://github.com/yamadashy/',
-  gitHubRepositoryUrl: 'https://github.com/yamadashy/tech-blog-rss-feed/',
+  author: 'cami-crane',
+  gitHubUserUrl: 'https://github.com/cami-crane/',
+  gitHubRepositoryUrl: 'https://github.com/cami-crane/tech-blog-rss-feed/',
 
   // Google Analytics系。フォークして使う際は値を空にするか書き換えてください
-  googleSiteVerification: 'GPLvXv8kYtLMW912ZS54DKFEZL6ruOrjOFLdHVTo37o',
-  globalSiteTagKey: 'G-CNNNTL0NB3',
+  googleSiteVerification: '',
+  globalSiteTagKey: '',
 
   // フィードの取得などに使う UserAgent
-  requestUserAgent: 'facebookexternalhit/1.1; yamadashy/tech-blog-rss-feed',
+  requestUserAgent: 'facebookexternalhit/1.1; cami-crane/tech-blog-rss-feed',
 
   // サイトの追加方法のリンク
   howToAddSiteLink:
-    'https://github.com/yamadashy/tech-blog-rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
+    'https://github.com/cami-crane/tech-blog-rss-feed#%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AE%E8%BF%BD%E5%8A%A0%E6%96%B9%E6%B3%95',
 };
