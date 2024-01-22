502fac2425d46ba31a34dd38e725cf1efbc7965d
Kazuki Yamada <koukun0120@gmail.com>
2024-01-22 23:52:15 +0900

    update: リダイレクトのテストはいらなくなる

--- tests/feed-info-list.test.ts
+++ tests/feed-info-list.test.ts
@@ -34,26 +34,3 @@ describe('フィードが取得可能', () => {
     );
   });
 });
-
-// リダイレクトされないテスト
-// rss-parser がリダイレクトするURLだとハングするため
-describe('リダイレクトされない', () => {
-  FEED_INFO_LIST.map((feedInfo: FeedInfo) => {
-    const testTitle = `${feedInfo.label} / ${feedInfo.url}`;
-    it.concurrent(
-      testTitle,
-      async () => {
-        const [error, response] = await to(fetch(feedInfo.url, { redirect: 'manual' }));
-        // リダイレクトしていないことを確認
-        expect(error).toBeNull();
-        expect(response).not.toBeNull();
-        if (response) {
-          const rerirectTo = response.headers.get('location');
-          expect(response.status, `Redirected to: ${rerirectTo}`).not.toEqual(301);
-          expect(response.status, `Redirected to: ${rerirectTo}`).not.toEqual(302);
-        }
-      },
-      180 * 1000,
-    );
-  });
-});
